2f4fbeef05182c39df5668951e01aeaa0be83e1d
Holoko Yurii <golovko.jura.g@gmail.com>
2019-04-17 21:14:52 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const csso = require('gulp-csso');
+const rename  =  require('gulp-rename');
+const concat  =  require('gulp-concat');
+var browserSync = require('browser-sync').create();
+var postcss      = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+// Static server
+gulp.task('browser', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./app"
+        }
+    });
+});
+
+
+gulp.task('sass', function () {
+    return gulp.src('./app/scss/main.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./app/css'));
+});
+
+
+gulp.task('allCss', function () { 
+    return gulp.src('./app/css/*.css')
+        .pipe(concat('allstyle.css'))
+        .pipe(postcss([ autoprefixer() ]))
+        .pipe(csso())
+        .pipe(rename({
+            suffix: '.min'
+          }))
+        .pipe(gulp.dest('./app/css'));
+});
+
+gulp.task('minCss', gulp.series('sass', 'allCss'))
+
+
+gulp.task('watch', function(){
+     gulp.watch('./app/scss/custom/*.scss', gulp.series('minCss'));
+     gulp.watch("app/*.html").on('change', browserSync.reload);
+     gulp.watch("app/css/*.css").on('change', browserSync.reload);
+
+})
+
+gulp.task('default', gulp.series('minCss', gulp.parallel('browser', 'watch')))
+
+
