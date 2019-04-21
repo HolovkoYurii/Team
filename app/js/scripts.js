ea929bca7ef45da1abfa02f7c43f73a24045dc8c
Holoko Yurii <golovko.jura.g@gmail.com>
2019-04-21 19:21:32 +0300

    block release-produts fixed bag, baner, trending-products

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -4,4 +4,16 @@ $( function() {
     $("#products-tabs").tabs();
   } );
 
+  $(document).ready(function(){
+    $('.trending-products-carousel').slick({
+      dots:true,
+      autoplay: true,
+      autoplaySpeed:5000,	
+      infinite: true,
+      slidesToShow: 3,
+      slidesthoshow:3,
+      
+    });
+  });
+
 
